49a9649b07a1c00fd45f49da2602168110968031
Pavel <Digiwo@gmail.com>
2026-08-20 20:16:23 +0200

    feat: add media event model

---
+++ src/MediaFlow.Core/Domain/MediaEvent.cs
@@ -0,0 +1,17 @@
+namespace MediaFlow.Core.Domain;
+
+public sealed class MediaEvent
+{
+    public Guid Id { get; init; } = Guid.NewGuid();
+    public required string Name { get; init; }
+    public string? Type { get; init; }
+    public DateTimeOffset StartAt { get; init; }
+    public DateTimeOffset? EndAt { get; init; }
+    public MediaEventStatus Status { get; init; } = MediaEventStatus.Planned;
+    public Guid SourceGroupId { get; init; }
+    public Guid DestinationShareId { get; init; }
+    public string DestinationFolderTemplate { get; init; } = "{event.name}";
+    public OperationMode OperationMode { get; init; } = OperationMode.SafeMove;
+    public ConflictStrategy ConflictStrategy { get; init; } = ConflictStrategy.AppendSourceName;
+    public DuplicateStrategy DuplicateStrategy { get; init; } = DuplicateStrategy.SafeMoveToExisting;
+}
