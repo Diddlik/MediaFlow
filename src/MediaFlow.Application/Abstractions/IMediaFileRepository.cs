064db4ebb311912879397b3187234d2a205f0fe7
Pavel <Digiwo@gmail.com>
2026-08-20 20:37:29 +0200

    feat: add media file repository abstraction

---
+++ src/MediaFlow.Application/Abstractions/IMediaFileRepository.cs
@@ -0,0 +1,10 @@
+using MediaFlow.Core.Domain;
+
+namespace MediaFlow.Application.Abstractions;
+
+public interface IMediaFileRepository
+{
+    Task<IReadOnlyList<MediaFile>> ListRecentAsync(int limit = 200, CancellationToken cancellationToken = default);
+    Task<MediaFile?> GetBySourceAsync(Guid sourceShareId, string sourcePath, CancellationToken cancellationToken = default);
+    Task UpsertAsync(MediaFile mediaFile, CancellationToken cancellationToken = default);
+}
