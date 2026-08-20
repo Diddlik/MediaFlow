863583bbc551417c5105443ec69b06df95485319
Pavel <Digiwo@gmail.com>
2026-08-20 21:58:10 +0200

    chore: remove unused storage options

--- src/MediaFlow.Application/Services/StorageSafetyOptions.cs
+++
@@ -1,8 +0,0 @@
-namespace MediaFlow.Application.Services;
-
-public sealed record StorageSafetyOptions(long MinimumFreeSpaceBytes)
-{
-    public const long DefaultMinimumFreeSpaceBytes = 512L * 1024L * 1024L;
-
-    public static StorageSafetyOptions Default { get; } = new(DefaultMinimumFreeSpaceBytes);
-}
